f260d1991ab5b048ca53e97d60e2be8bbef469ad
ResidentMario <aleksey.bilogur@gmail.com>
2018-11-10 13:51:54 -0800

    Initial test.

--- src/garbageman/tests/tests.py
+++ src/garbageman/tests/tests.py
@@ -1,6 +1,16 @@
 import unittest
+from shapely.geometry import LineString
+
+import sys; sys.path.append("../")
+import garbageman
 
 
-class TestBuildingsBlocksJoin(unittest.TestCase):
-    def setUp(self):
-        pass
+class TestCut(unittest.TestCase):
+    def testSimple(self):
+        input = LineString(((0, 0), (1, 0)))
+        expected = [LineString(((0, 0), (0.5, 0))), LineString(((0, 0.5), (1, 0)))]
+        result = garbageman.pipeline.cut(input, 0.5)
+
+        assert expected[0].equals(result[0])
+        import pdb; pdb.set_trace()
+        assert expected[1].equals(result[1])
